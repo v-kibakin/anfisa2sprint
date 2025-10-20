7ed04502594a0deb6a8bfdcb654c9f267d248070
Виктор Кибакин <v-kibakin@yandex.ru>
2025-10-20 16:01:55 +0300

    add localization to all ice cream models

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -8,14 +8,26 @@ class Category(PublishedModel):
     slug = models.SlugField(max_length=64, unique=True)
     output_order = models.PositiveSmallIntegerField(default=100)
 
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
 
 class Topping(PublishedModel):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
 
+    class Meta:
+        verbose_name = 'топинг'
+        verbose_name_plural = 'Топинги'
+
 
 class Wrapper(PublishedModel):
     title = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name = 'обертка'
+        verbose_name_plural = 'Обертки'
 
 
 class IceCream(PublishedModel):
@@ -36,3 +48,6 @@ class IceCream(PublishedModel):
     toppings = models.ManyToManyField(Topping)
     is_on_main = models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name = 'мороженое'
+        verbose_name_plural = 'Мороженое'
