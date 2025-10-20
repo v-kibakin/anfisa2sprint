c0c30993eba4d2423325eed34ead3155f1f84cf0
Виктор Кибакин <v-kibakin@yandex.ru>
2025-10-20 18:53:15 +0300

    add custom showing for ice cream admin

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -3,10 +3,25 @@ from .models import Category, Wrapper, Topping, IceCream
 
 
 class IceCreamAdmin(admin.ModelAdmin):
-    ...
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',) 
+    list_filter = ('category',)
+    list_display_links = ('title',)
 
 
-admin.register(IceCream, IceCreamAdmin)
+admin.site.register(IceCream, IceCreamAdmin)
 
 admin.site.register(Category)
 admin.site.register(Wrapper)
