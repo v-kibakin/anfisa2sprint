bf02c6450524027fe2c921bb1e1f2430e7154e15
Виктор Кибакин <v-kibakin@yandex.ru>
2025-10-20 15:58:31 +0300

    add localization to ice cream catalog

--- anfisa_for_friends/ice_cream/apps.py
+++ anfisa_for_friends/ice_cream/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class IceCreamConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ice_cream'
+
+    verbose_name = 'Каталог мороженого'
