0760794edc4b8da671caa22348baeffe44e06219
Виктор Кибакин <v-kibakin@yandex.ru>
2025-10-20 15:51:36 +0300

    register all of ice cream models

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Wrapper, Topping, IceCream
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Wrapper)
+admin.site.register(Topping)
+admin.site.register(IceCream)
